e5f17bb34327339bfea3cff99c75c5450afcc14f
MatteoCampa <matteo.campagnaro.1@studenti.unipd.it>
2024-05-18 12:11:28 +0200

    Validazione dell'input #6
    Gestione degli errori di inserimento
    
    close #6

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,7 +7,18 @@ package it.unipd.mtss;
 import it.unipd.mtss.exception.InvalidNumber;
 
 public class IntegerToRoman {
-    public static String convert(int number){
+    public static String convert(int number) throws InvalidNumber {
+        validaArgomenti(number);
+
         return null;
+    }
+
+    private static void validaArgomenti(int number) throws InvalidNumber{
+        if(number>1000) {
+            throw new InvalidNumber("Numero Troppo Grande");
+        }
+        else if (number<1) {
+            throw new InvalidNumber("Numero Troppo Piccolo");
+        }
     }
 }
