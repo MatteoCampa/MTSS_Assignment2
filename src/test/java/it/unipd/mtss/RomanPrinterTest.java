a82ed47df0d6125dae9ea728e8129f95a7c6c475
MatteoCampa <matteo.campagnaro.1@studenti.unipd.it>
2024-05-18 12:54:10 +0200

    Implementazione conversione (I V X) #10
    Creazione dei test e del codice per convertire una stringa che contiene le lettere: I V X, in asciart
    
    close #10

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -16,7 +16,51 @@ public class RomanPrinterTest {
     }
 
     @Test
-    public void test(){
-        assertTrue(true);
+    public void testPrint_Ten() throws InvalidNumber {
+        String expected = "                   \n" +
+                "`8.`8888.      ,8' \n" +
+                " `8.`8888.    ,8'  \n" +
+                "  `8.`8888.  ,8'   \n" +
+                "   `8.`8888.,8'    \n" +
+                "    `8.`88888'     \n" +
+                "    .88.`8888.     \n" +
+                "   .8'`8.`8888.    \n" +
+                "  .8'  `8.`8888.   \n" +
+                " .8'    `8.`8888.  \n" +
+                ".8'      `8.`8888. \n";
+        assertEquals(expected, romanPrinter.print(10));
     }
+
+    @Test
+    public void testPrint_Five() throws InvalidNumber {
+        String expected = "                       \n" +
+                "`8.`888b           ,8' \n" +
+                " `8.`888b         ,8'  \n" +
+                "  `8.`888b       ,8'   \n" +
+                "   `8.`888b     ,8'    \n" +
+                "    `8.`888b   ,8'     \n" +
+                "     `8.`888b ,8'      \n" +
+                "      `8.`888b8'       \n" +
+                "       `8.`888'        \n" +
+                "        `8.`8'         \n" +
+                "         `8.`          \n";
+        assertEquals(expected, romanPrinter.print(5));
+    }
+
+    @Test
+    public void testPrint_One() throws InvalidNumber {
+        String expected = "        \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n" +
+                " 8 8888 \n";
+        assertEquals(expected, romanPrinter.print(1));
+    }
+    
 }
