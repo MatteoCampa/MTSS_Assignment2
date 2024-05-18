8c3aef9b19ef91dd2d25953228d5e51dab3ce5f4
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2024-05-18 13:07:05 +0200

    Implementazione conversione (L C D M) #11
    Creazione dei test e del codice per convertire una stringa che contiene le lettere: L C D M, in asciart
    
    close #11

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -16,6 +16,70 @@ public class RomanPrinterTest {
     }
 
     @Test
+    public void testPrint_Thousand() throws InvalidNumber {
+        String expected = "          .         .           \n" +
+                "         ,8.       ,8.          \n" +
+                "        ,888.     ,888.         \n" +
+                "       .`8888.   .`8888.        \n" +
+                "      ,8.`8888. ,8.`8888.       \n" +
+                "     ,8'8.`8888,8^8.`8888.      \n" +
+                "    ,8' `8.`8888' `8.`8888.     \n" +
+                "   ,8'   `8.`88'   `8.`8888.    \n" +
+                "  ,8'     `8.`'     `8.`8888.   \n" +
+                " ,8'       `8        `8.`8888.  \n" +
+                ",8'         `         `8.`8888. \n";
+        assertEquals(expected, romanPrinter.print(1000));
+    }
+
+    @Test
+    public void testPrint_FiveHundred() throws InvalidNumber {
+        String expected = "                   \n" +
+                "8 888888888o.      \n" +
+                "8 8888    `^888.   \n" +
+                "8 8888        `88. \n" +
+                "8 8888         `88 \n" +
+                "8 8888          88 \n" +
+                "8 8888          88 \n" +
+                "8 8888         ,88 \n" +
+                "8 8888        ,88' \n" +
+                "8 8888    ,o88P'   \n" +
+                "8 888888888P'      \n";
+        assertEquals(expected, romanPrinter.print(500));
+    }
+
+    @Test
+    public void testPrint_OneHundres() throws InvalidNumber {
+        String expected = "                  \n" +
+                "    ,o888888o.    \n" +
+                "   8888     `88.  \n" +
+                ",8 8888       `8. \n" +
+                "88 8888           \n" +
+                "88 8888           \n" +
+                "88 8888           \n" +
+                "88 8888           \n" +
+                "`8 8888       .8' \n" +
+                "   8888     ,88'  \n" +
+                "    `8888888P'    \n";
+        assertEquals(expected, romanPrinter.print(100));
+    }
+
+    @Test
+    public void testPrint_Fifty() throws InvalidNumber {
+        String expected = "               \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 8888         \n" +
+                "8 888888888888 \n";
+        assertEquals(expected, romanPrinter.print(50));
+    }
+
+    @Test
     public void testPrint_Ten() throws InvalidNumber {
         String expected = "                   \n" +
                 "`8.`8888.      ,8' \n" +
@@ -61,6 +125,22 @@ public class RomanPrinterTest {
                 " 8 8888 \n" +
                 " 8 8888 \n";
         assertEquals(expected, romanPrinter.print(1));
+    }
+
+    @Test
+    public void testPrint_AllLetters() throws InvalidNumber {
+        String expected = "                                                                                                      \n" +
+                "8 888888888o.          ,o888888o.    8 8888         `8.`8888.      ,8' `8.`888b           ,8'  8 8888 \n" +
+                "8 8888    `^888.      8888     `88.  8 8888          `8.`8888.    ,8'   `8.`888b         ,8'   8 8888 \n" +
+                "8 8888        `88. ,8 8888       `8. 8 8888           `8.`8888.  ,8'     `8.`888b       ,8'    8 8888 \n" +
+                "8 8888         `88 88 8888           8 8888            `8.`8888.,8'       `8.`888b     ,8'     8 8888 \n" +
+                "8 8888          88 88 8888           8 8888             `8.`88888'         `8.`888b   ,8'      8 8888 \n" +
+                "8 8888          88 88 8888           8 8888             .88.`8888.          `8.`888b ,8'       8 8888 \n" +
+                "8 8888         ,88 88 8888           8 8888            .8'`8.`8888.          `8.`888b8'        8 8888 \n" +
+                "8 8888        ,88' `8 8888       .8' 8 8888           .8'  `8.`8888.          `8.`888'         8 8888 \n" +
+                "8 8888    ,o88P'      8888     ,88'  8 8888          .8'    `8.`8888.          `8.`8'          8 8888 \n" +
+                "8 888888888P'          `8888888P'    8 888888888888 .8'      `8.`8888.          `8.`           8 8888 \n";
+        assertEquals(expected, romanPrinter.print(666));
     }
     
 }
